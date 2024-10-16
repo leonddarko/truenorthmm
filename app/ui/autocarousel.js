ededcca6408aec45c0f4b670791442a529fa5520
leonddarko <leonddarko@gmail.com>
2024-10-16 11:14:33 +0000

    Ui update

--- app/ui/autocarousel.js
+++ app/ui/autocarousel.js
@@ -16,11 +16,11 @@ export default function AutoEmblaCarousel() {
 
     const autoPlaySlideContent = [{
         id: 0,
-        bImage: "bg-[url('/assets/mediamonitoringbg.jpeg')]",
+        bImage: "bg-[url('/assets/focused_group_1.0.jpeg')]",
     },
     {
         id: 1,
-        bImage: "bg-[url('/assets/mediamonitoringbg.jpeg')]",
+        bImage: "bg-[url('/assets/Duo_focused.jpeg')]",
     },
     {
         id: 2,
@@ -33,7 +33,7 @@ export default function AutoEmblaCarousel() {
     return (
         <>
             <div
-                className={`relative overflow-hidden pr-6 mt-20 pt-1 ${grab} transition-all`} ref={emblaRef}
+                className={`relative overflow-hidden pr-4 mt-20 pt-1 ${grab} transition-all`} ref={emblaRef}
                 onMouseDown={() => grab === "cursor-grab" && setGrab("cursor-grabbing")}
                 onMouseUp={() => grab === "cursor-grabbing" && setGrab("cursor-grab")}
             >
@@ -41,12 +41,12 @@ export default function AutoEmblaCarousel() {
                     {autoPlaySlideContent.map((item) => (
                         <div key={item.id} className="embla__slide2">
                             <div
-                                className={`px-6 md:px-16 pb-16 h-screen flex justify-start items-end z-0 bg-black 
+                                className={`px-6 md:px-16 h-screen flex justify-start items-end z-0 bg-black 
                                     ${item.bImage}
                                     bg-no-repeat bg-cover bg-top rounded-3xl shadow`}
                             >
-                                <div>
-                                    <h1 className="text-white font-notoserif text-5xl md:text-7xl font-bold max-w-3xl">
+                                <div className="mb-28">
+                                    <h1 className="text-white font-notoserif text-4xl md:text-6xl font-bold max-w-3xl">
                                         Monitoring. Evaluation and Measurement.
                                     </h1>
                                     <p className="text-white font-notosans font-medium">
